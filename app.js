4f7908958d70a4a3542e76c2de0d0891e307796f
Kyle VanderBeek <kylev@kylev.com>
2013-07-17 23:28:41 -0700

    Up and running with Jade and basic routes

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-var express = require('express');
-var app = express();
+var http = require('http'),
+  express = require('express'),
+  jade = require('jade');
 
-app.get('/hello.txt', function(req, res){
-  var body = "Hello World";
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+var routes = require('./routes');
+
+var app = module.exports.app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.favicon());
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+app.get('/', routes.index)
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-app.get('/hello2.txt', function(req, res){
-  res.send('Hello again');
-});
-
-app.listen(3000);
-console.log('Listening on port 3000');
